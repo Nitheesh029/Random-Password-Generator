4fe69b08ebeab092bc73106679d0d81874aa65ce
S Nitheesh <qxnitheesh@gmail.com>
2024-12-21 21:42:31 +0530

    Generating a random pass with js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Happy developing ✨')
+const passwordBox = document.getElementById('password');
+const length = 15;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = upperCase.toLowerCase();
+const numbers = "0123456789";
+const symbols = "!@#$%";
+
+const allChars = lowerCase + upperCase + symbols + numbers;
+
+function generatePassword(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random()*upperCase.length)];
+    password += lowerCase[Math.floor(Math.random()*lowerCase.length)];
+    password += numbers[Math.floor(Math.random()*numbers.length)];
+    password += symbols[Math.floor(Math.random()*symbols.length)];
+
+    while(length > password.length){
+    password += allChars[Math.floor(Math.random()*allChars.length)];
+    }
+    passwordBox.value = password;
+}
+
+generatePassword();
